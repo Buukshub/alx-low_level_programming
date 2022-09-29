b393c29889f6a03f599735dd95b32a6ac3ba6d9a
Buukshub <mickzystar@gmail.com>
2022-09-28 17:36:50 -0700

    Everything about this task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string and adds a newline using recursion
+ *_puts_recursion - prints a string and adds a newline using recursion
  *
- * @s: pointer that has the address of the string to be printed
+ *@s: pointer that has the address of the string to be printed
  *
- * Return: nothing
+ *Return: nothing
  */
 void _puts_recursion(char *s)
 {
